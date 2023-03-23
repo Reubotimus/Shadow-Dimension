47e4ef5d09e73db8f92ba394ee0ba63eebfc3a27
Reubotimus <reubencook555@gmail.com>
2023-03-24 10:30:47 +1100

    added readme and UML diagram

---
+++ README.md
@@ -0,0 +1,4 @@
+# Shadow Dimension
+
+This project is a 2D game written in Java using a basic graphics library (BAGEL).
+The focus on this project was to utilise object oriented principles to design and create a maintainable and extendable game. Please see the UML.pdf document for a UML diagram made at the start of the project.
--- src/IntermediateScreen.java
+++ src/IntermediateScreen.java
@@ -3,19 +3,27 @@ import bagel.Keys;
 import bagel.Window;
 
 public class IntermediateScreen extends Screen {
+    private long intialisationTime;
+
+    public IntermediateScreen() {
+        this.intialisationTime = System.currentTimeMillis();
+    }
+
     private static final String[] lines = {"PRESS SPACE TO START",
             "PRESS A TO ATTACK",
             "DEFEAT NAVEC TO WIN"};
 
     @Override
     public Screen update(Input input) {
-        SUBTITLE_FONT.drawString(lines[0], 350, 350);
-        SUBTITLE_FONT.drawString(lines[1],
-                (double)(Window.getWidth() / 2) - SUBTITLE_FONT.getWidth(lines[1]) / 2.0,
-                350 + 45);
-        SUBTITLE_FONT.drawString(lines[2],
-                (double)(Window.getWidth() / 2) - SUBTITLE_FONT.getWidth(lines[2]) / 2.0,
-                350 + 2 * 45);
+        if (System.currentTimeMillis() - this.intialisationTime > 3000) {
+            SUBTITLE_FONT.drawString(lines[0], 350, 350);
+            SUBTITLE_FONT.drawString(lines[1],
+                    (double) (Window.getWidth() / 2) - SUBTITLE_FONT.getWidth(lines[1]) / 2.0,
+                    350 + 45);
+            SUBTITLE_FONT.drawString(lines[2],
+                    (double) (Window.getWidth() / 2) - SUBTITLE_FONT.getWidth(lines[2]) / 2.0,
+                    350 + 2 * 45);
+        } else {
         if (input.wasPressed(Keys.SPACE)) {
             return new Level(1);
         }
